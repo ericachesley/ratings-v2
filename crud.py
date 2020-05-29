2d6ff8774cb09e83a7454cdfd15941f9e8f09504
Erica Chesley <erchesley@gmail.com>
2020-05-29 13:03:17 -0700

    Create functions and seed database implemented

--- crud.py
+++ crud.py
@@ -1,11 +1,11 @@
 from model import *
-import json
 
 def create_user(email, password):
     new_user = User(email=email, password=password)
     db.session.add(new_user)
     db.session.commit()
     return new_user
+
 
 def create_movie(title, overview, release_date, poster_path):
     new_movie = Movie(title=title, 
@@ -16,19 +16,13 @@ def create_movie(title, overview, release_date, poster_path):
     db.session.commit()
     return new_movie
 
+
 def create_rating(user, movie, score):
     new_rating = Rating(user=user, movie=movie, score=score)
     db.session.add(new_rating)
     db.session.commit()
     return new_rating
 
-# def enter_movies():
-#     movies = json.load(open('data/movies.json'))
-#     for movie in movies:
-#         create_movie(movie['title'], 
-#                      movie['overview'], 
-#                      movie['release_date'], 
-#                      movie['poster_path'])
 
 
 if __name__ == '__main__':
